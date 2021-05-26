e0de35da769c0839047e32934131f102b645c76d
Gildea <gildem01@bigpurple-ln3.cm.cluster>
2021-05-26 16:42:23 -0400

    added snakemake report and multiQC

--- snakemake_init.sh
+++ snakemake_init.sh
@@ -11,8 +11,8 @@ do
 done
 
 snakemake --cluster "sbatch -J {cluster.Job_name} --mem {cluster.Mem} -c {cluster.Cores} -p {cluster.Partition} -t {cluster.Time} --output {cluster.Error}" --cluster-config cluster_config.yml --jobs 13 
-#--report snake_make_report.html
+snakemake --report snake_make_report.html
 conda deactivate
 module unload miniconda3/4.6.14
 module load python/cpu/2.7.15
-#multiqc .
+multiqc .
