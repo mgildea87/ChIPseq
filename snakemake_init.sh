6bb27bc5cf636da776602ef6af8a08301f1fcda6
Gildea <gildem01@bigpurple-ln2.cm.cluster>
2021-06-23 14:51:31 -0400

    updated miniconda and multiqc version

--- snakemake_init.sh
+++ snakemake_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-module load miniconda3/4.6.14
+module load miniconda3/cpu/4.9.2
 conda activate /gpfs/data/fisherlab/conda_envs/ChIPseq
 
 for num in {1..100}
@@ -12,7 +12,4 @@ done
 
 snakemake --cluster "sbatch -J {cluster.Job_name} --mem {cluster.Mem} -c {cluster.Cores} -p {cluster.Partition} -t {cluster.Time} --output {cluster.Error}" --cluster-config cluster_config.yml --jobs 13 
 snakemake --report snake_make_report.html
-conda deactivate
-module unload miniconda3/4.6.14
-module load python/cpu/2.7.15
 multiqc .
